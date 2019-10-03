4e4a84999bf40d01860b25f8af51847cbf74d707
Jackie Atwood <takalaj@verizon.net>
2019-10-03 11:08:00 -0400

    MVP met, toggle switch functional, dark mode implemented

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,19 +1,19 @@
-// import React, { useEffect } from "react";
-import useLocalStorage from "./useLocalStorage";
+import React, { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
 
 export const useDarkMode = () => {
-    const [someValue, setSomeValue] = useLocalStorage("key here")
-}
 
+    const [value, setValue] = useLocalStorage("dark-mode");
 
-// add a class to the body?
-// useEffect?
-// useLocalStorage true or false
-// if true add dark mode to body element
-// if false, remove from the body element
+    useEffect(() => {
+    const body = window.document.body
+        if (value) {
+            body.classList.add("dark-mode");
+        } else {
+            body.classList.remove("dark-mode")
+        }
+    }, [value])
 
-// setter function to toggle dark modeforward the value and the setter that were returned out of the local out useLocalStorage
-
-// import darkmode to the navbar component
-// replace out hooks in navabr? click toggle
-// 
+return [value, setValue];
+};
